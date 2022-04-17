2de7018e428c95bcc65c60c5f3c864d16e091311
Vincent-Kwy <71835061+Vincent-Kwy@users.noreply.github.com>
2022-04-16 19:26:14 -0500

    Update sql

--- create.sql
+++ create.sql
@@ -1,7 +1,43 @@
 DROP TABLE IF EXISTS Categories;
+DROP TABLE IF EXISTS Items;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Bids;
+
+CREATE TABLE Items(
+    Item varchar(256)
+    Currently money,
+    Buy_Price money,
+    Forst_Bid money,
+    Number_of_Bids INT,
+    Starts datetime,
+    Ends datetime,
+    Description varchar(256)
+    Primary key (ItemID),
+    Foreign key (Seller) references Users (UserID)
+);
+
 
 CREATE TABLE Categories(
     ItemID   INT,
     CategoryNames  varchar(256)
-    
+    Primary key (ItemID, CategoryNames),
+    Foreign key (ItemID) references Items (ItemID)
+);
+
+CREATE TABLE Users(
+    Rating INT,
+    Country varchar(256),
+    User_Address varchar(256)
+    Primary key (UserID)
+);
+
+CREATE TABLE Bids(
+    ItemID   INT,
+    Bidder  varchar(256),
+    BidID INT,
+    BidTime datetime,
+    Amount money
+    Primary key (BidID),
+    Foreign key (Bidder) references Users (UserID),
+    Foreign key (ItemID) references Items (ItemID)
 );
